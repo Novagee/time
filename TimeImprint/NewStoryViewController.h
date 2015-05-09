033b101bd4a212e15da971a7add8e1e464afebb3
Kelvin Lin <fenglam09@gmail.com>
2015-05-09 00:52:56 -1000

    New Story Screen

---
+++ TimeImprint/NewStoryViewController.h
@@ -0,0 +1,13 @@
+//
+//  NewStoryViewController.h
+//  TimeImprint
+//
+//  Created by Kelvin Lam on 5/9/15.
+//  Copyright (c) 2015 Timeimprint. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewStoryViewController : UIViewController
+
+@end
