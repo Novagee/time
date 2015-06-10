cdbb83773101d6239d6644e8cce9fc7f20270490
Kelvin Lin <fenglam09@gmail.com>
2015-06-09 21:39:54 -1000

    Remove old codes

--- Pods/Target Support Files/Pods-TimeImprint-JDFPeekaboo/Pods-TimeImprint-JDFPeekaboo-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TimeImprint_JDFPeekaboo : NSObject
-@end
-@implementation PodsDummy_Pods_TimeImprint_JDFPeekaboo
-@end
--- Pods/Target Support Files/Pods-TimeImprint-PBJVision/Pods-TimeImprint-PBJVision-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TimeImprint_PBJVision : NSObject
-@end
-@implementation PodsDummy_Pods_TimeImprint_PBJVision
-@end
